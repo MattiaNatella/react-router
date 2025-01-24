ebec0754bac5b9344667db82bcaa70d48b292d44
MattiaNatella <mattianatella@gmail.com>
2025-01-24 12:19:31 +0100

    [add] post detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AboutUs from "./pages/AboutUs"
 import HomePage from "./pages/HomePage"
 import PostsList from "./pages/PostsList"
+import PostDetail from "./pages/PostDetail"
 import DefaultLayout from "./layouts/DefaultLayout"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" Component={HomePage} />
           <Route path="/chi-siamo" Component={AboutUs} />
           <Route path="/posts" Component={PostsList} />
-          {/* <Route path="/dettaglio-post/:id" Component={PostDetail} /> */}
+          <Route path="/dettaglio-post/:id" Component={PostDetail} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/PostDetail.jsx
@@ -0,0 +1,16 @@
+// import { useState, useEffect } from 'react'
+// import { useNavigate, useParams } from 'react-router-dom'
+
+
+const PostDetail = () => {
+    // const navigate = useNavigate()
+    // const { id } = useParams();
+
+
+
+    return (
+        <div>PostDetail</div>
+    )
+}
+
+export default PostDetail
