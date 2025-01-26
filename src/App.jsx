3e3df92e60c22ed33d7f81eff6bc0efc5bc64b76
MattiaNatella <mattianatella@gmail.com>
2025-01-26 18:30:11 +0100

    [add] notFound page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import HomePage from "./pages/HomePage"
 import PostsList from "./pages/PostsList"
 import PostDetail from "./pages/PostDetail"
 import DefaultLayout from "./layouts/DefaultLayout"
-
+import NotFound404 from "./pages/NotFound404"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/chi-siamo" Component={AboutUs} />
           <Route path="/posts" Component={PostsList} />
           <Route path="/dettaglio-post/:id" Component={PostDetail} />
+          <Route path="*" Component={NotFound404} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/NotFound404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound404 = () => {
+    return (
+        <h1 className="container d-flex justify-content-center border border-warning border-4 bg-info rounded my-5">404 Not Found</h1>
+    )
+}
+
+export default NotFound404
